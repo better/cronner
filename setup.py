ba378b310ecb32aae335d23043cd3f963ee6e997
Liam Buchanan <liam.buchanan@gmail.com>
2018-01-17 20:53:18 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='cronner',
+    version='0.1.0',
+    description='Easily schedule python methods to be run under the cron daemon.',
+    py_modules=['cronner'],
+    url='https://github.com/liambuchanan/cronner',
+    author='Liam Buchanan',
+    author_email='liam.buchanan@gmail.com',
+    license='MIT',
+    keywords='cron crontab',
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, <4'
+)
